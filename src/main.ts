12a2fca2169c52e70a1fe443bec29975b456ca60
Bryan Mejia Ruiz <bryanmejiaruiz@MacBook-Pro-de-Bryan.local>
2026-02-25 09:51:20 -0600

    chore: commit all current changes

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   // Enable CORS
   app.enableCors({
     origin: [
-      process.env.FRONTEND_URL || 'http://localhost:3000',
+      'http://localhost:3000',
       'https://crmmibofrontend-production.up.railway.app',
     ],
     methods: 'GET,POST,PATCH,DELETE,OPTIONS',
